e851adcf422d655eb82e8e99a315a05928af9144
Abhishek Mishra <abhishekmishra1022@gmail.com>
2026-07-16 00:02:49 +0530

    first commit

---
+++ src/test/java/com/example/interview_prep/InterviewPrepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.interview_prep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewPrepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
